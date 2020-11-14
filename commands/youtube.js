867bad32946e4fe6f32b23a32afebbf44572a5bb
Irbyk <irb.byk@gmail.com>
2020-11-14 17:14:28 -0500

    Fix youtube & update dicordjs
    Fix youtube command and update to dicordjs v12.3

--- commands/youtube.js
+++ commands/youtube.js
@@ -7,7 +7,7 @@ module.exports = {
     description : 'play the youtube url to the server(audio only).',
     process : async function(bot, client, message, query) {
       ytdl.getInfo(query).then(function(songInfo) {
-        bot.songQueue.push({'artist' : songInfo.author.name , 'title': songInfo.title, 'url': songInfo.video_url});
+        bot.songQueue.push({'artist' : songInfo.videoDetails.author.name , 'title': songInfo.videoDetails.title, 'url': songInfo.videoDetails.video_url});
         if(bot.isPlaying){
           message.reply(bot.language.YOUTUBE_SUCCES.format({artist : songInfo.author.name, title : songInfo.title, caracteres_commande : bot.config.caracteres_commande}));
         } else {
