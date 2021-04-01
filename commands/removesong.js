07d7bd0ede06612118dce57a129d2ee5392d439c
leukosaima <paul_filion@mac.com>
2021-04-01 15:54:09 -0400

    Update removesong.js

--- commands/removesong.js
+++ commands/removesong.js
@@ -9,7 +9,10 @@ module.exports = {
       songNumber = songNumber - 1;
 
       if (bot.songQueue.length > 0 ) {
-        if (songNumber > -1 && songNumber <= bot.songQueue.length) {
+        if (bot.isPlaying && songNumber == 0) {
+          message.reply(bot.language.REMOVESONG_ERROR);
+        }
+        else if (songNumber > -1 && songNumber <= bot.songQueue.length) {
           let removedSong = bot.songQueue.splice(songNumber, 1);
           // message that it has been removed
           message.reply(bot.language.REMOVESONG_SUCCES.format({artist : removedSong[0].artist, title : removedSong[0].title}));
