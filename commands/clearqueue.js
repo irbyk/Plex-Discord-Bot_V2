6c2034d65e349a830415a7e51e658f5fa989f8d1
leukosaima <paul_filion@mac.com>
2021-04-01 14:22:26 -0400

    Update clearqueue.js

--- commands/clearqueue.js
+++ commands/clearqueue.js
@@ -5,8 +5,13 @@ module.exports = {
     description: 'clears all songs in queue',
     process: function(bot, client, message) {
       if (bot.songQueue.length > 0) {
-        bot.songQueue = []; // remove all songs from queue
-
+        let newQueue = [];
+        if (bot.isPlaying) {
+          newQueue[0] = bot.songQueue[0];
+        }
+        
+        bot.songQueue = newQueue;
+        
         message.reply(bot.language.CLEARQUEUE_SUCCES);
       }
       else {
