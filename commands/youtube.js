6738185b35b240547da073babe1ad08012a5b75e
leukosaima <paul_filion@mac.com>
2021-04-06 17:57:28 -0400

    Update youtube.js

--- commands/youtube.js
+++ commands/youtube.js
@@ -9,7 +9,7 @@ module.exports = {
       ytdl.getInfo(query).then(function(songInfo) {
         bot.songQueue.push({'artist' : songInfo.videoDetails.author.name , 'title': songInfo.videoDetails.title, 'url': songInfo.videoDetails.video_url});
         if(bot.isPlaying){
-          message.reply(bot.language.YOUTUBE_SUCCES.format({artist : songInfo.author.name, title : songInfo.title, caracteres_commande : bot.config.caracteres_commande}));
+          message.reply(bot.language.YOUTUBE_SUCCES.format({artist : songInfo.videoDetails.author.name, title : songInfo.videoDetails.title, caracteres_commande : bot.config.caracteres_commande}));
         } else {
           bot.playSong(message);
         }
